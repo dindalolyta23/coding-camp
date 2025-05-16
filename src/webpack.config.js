088a10dfc8d89b6c5cd2a373cd5cc88c662e88f9
dindalolyta23 <dindalolyta@students.amikom.ac.id>
2025-05-17 01:26:42 +0700

    Initial commit after modification

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,7 +1,10 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
-const { GenerateSW } = require('workbox-webpack-plugin');
+const { GenerateSW } = require("workbox-webpack-plugin");
+const CompressionPlugin = require("compression-webpack-plugin");
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
+
 module.exports = {
   entry: {
     app: path.resolve(__dirname, "src/scripts/index.js"),
@@ -9,20 +12,28 @@ module.exports = {
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
+    clean: true,
   },
   module: {
     rules: [
       {
-        test: /\.(png|jpe?g|gif)$/i,
+        test: /\.(png|jpe?g|gif|svg)$/i,
         type: "asset/resource",
+        generator: {
+          filename: "images/[name][ext]",
+        },
       },
       {
-        test: /\.js$/,                // Menangani file .js
-        exclude: /node_modules/,      // Tidak memproses file di dalam node_modules
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
         use: {
-          loader: "babel-loader",     // Gunakan babel-loader untuk transpile JavaScript
+          loader: "babel-loader",
           options: {
-            presets: ["@babel/preset-env"],  // Gunakan preset-env untuk mendukung fitur ES6+
+            presets: ["@babel/preset-env"],
           },
         },
       },
@@ -31,18 +42,58 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "src/index.html"),
+      filename: "index.html",
+      inject: "body",
     }),
     new CopyWebpackPlugin({
       patterns: [
         {
-          from: path.resolve(__dirname, "src/public/"),
-          to: path.resolve(__dirname, "dist/"),
+          from: path.resolve(__dirname, "src/public"),
+          to: path.resolve(__dirname, "dist"),
+          globOptions: {
+            ignore: ["**/index.html"],
+          },
         },
       ],
     }),
     new GenerateSW({
       clientsClaim: true,
       skipWaiting: true,
+      additionalManifestEntries: [
+        { url: "/", revision: "1" },
+      ],
+      runtimeCaching: [
+        {
+          urlPattern: /^https:\/\/story-api\.dicoding\.dev/,
+          handler: "StaleWhileRevalidate",
+          options: {
+            cacheName: "api-cache",
+            expiration: {
+              maxEntries: 50,
+              maxAgeSeconds: 24 * 60 * 60,
+            },
+          },
+        },
+        {
+          urlPattern: /\.(png|jpg|jpeg|gif|svg)$/,
+          handler: "CacheFirst",
+          options: {
+            cacheName: "image-cache",
+            expiration: {
+              maxEntries: 100,
+              maxAgeSeconds: 30 * 24 * 60 * 60,
+            },
+          },
+        },
+      ],
+    }),
+    new CompressionPlugin({
+      algorithm: "gzip",
+      test: /\.(js|css|html|json|ico|svg|eot|ttf|woff)$/,
+    }),
+    new BundleAnalyzerPlugin({
+      analyzerMode: "static", // Hasil akan disimpan di file HTML dan dibuka otomatis
+      openAnalyzer: false, // Jangan buka otomatis (bisa jadi true kalau mau langsung lihat)
     }),
   ],
 };
